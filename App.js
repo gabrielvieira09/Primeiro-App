efc4a28548b0b2c6b689551481b6dc4d3a84aff9
Gabriel_Vieira <gabrielafonsovieira09@gmail.com>
2024-02-22 10:28:41 -0300

    Criacao de pagina inicial (instagram)

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
-import logo from './assets/Logo.png';
-import { Feather } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons';
+import Header from "./src/components/Header";
 
 export default function App() {
   return (
-    <View>
-      <View style={styles.container}>
-      <Image style={{width:150, height:50}} source={logo} />
-
-      <View style={{flexDirection: 'row'}}>
-      <TouchableOpacity >
-        <Feather style={{marginRight: 25}} name="heart" size={24} color="black" />
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <FontAwesome5 style={{}} name="facebook-messenger" size={24} color="black" />
-      </TouchableOpacity>
-      </View>
-      </View>
+    <View 
+      style={{
+        marginVertical: 32
+      }}
+    >
+      <Header />
       <ScrollView 
         contentContainerStyle={{gap:8}}
         horizontal
@@ -47,14 +37,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    marginHorizontal: 17.5,
-    marginBottom: 10,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    flexDirection: 'row'
-  },
-});
---
+++ src/components/Header/index.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import logo from '../../assets/Logo.png';
+import { Feather } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+export default function Header() {
+    return (
+
+        <View style={styles.container}>
+            <Image style={{ width: 150, height: 50 }} source={logo} />
+
+            <View style={{ flexDirection: 'row' }}>
+                <TouchableOpacity >
+                    <Feather style={{ marginRight: 25 }} name="heart" size={24} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <FontAwesome5 style={{}} name="facebook-messenger" size={24} color="black" />
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+
+  
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        marginHorizontal: 17.5,
+        marginBottom: 10,
+        marginTop: 20,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row'
+    },
+});
